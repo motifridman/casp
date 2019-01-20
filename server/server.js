0d1a5b3efeaddb2fe9ddaa59369cd22516451e4e
unknown <motifridman5@gmail.com>
2019-01-20 15:15:45 +0200

    Server fix

--- server/server.js
+++ server/server.js
@@ -6,12 +6,33 @@ const bodyParser = require('body-parser');
 const { ObjectID } = require('mongodb');
 
 var { mongoose } = require('./db/mongoose');
+var {Contact} = require('./models/contact')
 
 var app = express();
 const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
+app.post('/contacts', (req, res) => {
+    var contact = new Contact({
+        name: req.body.name,
+        number: req.body.number
+    });
+
+    contact.save().then((doc) => {
+        res.send(doc);
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
+
+app.get('/contacts', (req, res) => {
+    Contact.find().then((contacts) => {
+        res.send({ contacts });
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
 
 app.listen(port, () => {
     console.log(`Started up at port ${port}`)
