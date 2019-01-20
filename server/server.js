edb1ff49a011b3ca37e2cadc2d21303164f20c0d
unknown <motifridman5@gmail.com>
2019-01-20 16:51:48 +0200

    Add more routes

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 const { ObjectID } = require('mongodb');
 
 var { mongoose } = require('./db/mongoose');
-var {Contact} = require('./models/contact')
+var { Contact } = require('./models/contact')
 
 var app = express();
 const port = process.env.PORT;
@@ -19,18 +19,10 @@ app.post('/contacts', (req, res) => {
         number: req.body.number
     });
 
-    // Contact.findOne({name: contact.name}).then((doc) =>
-    // {
-    //     throw new Error(`The name ${contact.name} is already exist`);
-    //     //res.status(404).send({error: `The name ${contact.name} is already exist`, doc})
-    // }, (doc) =>{
-    //     Contact.findOne({})
-    // })
-
     contact.save().then((doc) => {
         res.send(doc);
     }, (e) => {
-        res.status(400).send(e);
+        res.status(400).send(e.errmsg);
     });
 });
 
@@ -38,8 +30,42 @@ app.get('/contacts', (req, res) => {
     Contact.find().then((contacts) => {
         res.send({ contacts });
     }, (e) => {
-        res.status(400).send(e);
+        res.status(400).send(e.errmsg);
     });
+});
+
+app.delete('/contacts/:name', (req, res) => {
+    Contact.findOneAndDelete({ name: req.params.name }).then((contact) => {
+        if (!contact) {
+            return res.status(404).send();
+        }
+        res.status(200).send({ contact });
+    }).catch((e) => {
+        res.status(400).send();
+    });
+});
+
+app.patch('/contacts/:name', (req, res) => {
+    var name = req.params.name;
+    var body = _.pick(req.body, ['name', 'number']);
+
+    Contact.findOneAndUpdate({name}, { $set: body }, { new: true }).then((contact) => {
+        if (!contact) {
+            return res.status(404).send();
+        }
+
+        res.send({ contact });
+    }).catch((e) => {
+        res.status(400).send();
+    });
+});
+
+app.get('/status', (req, res) =>{
+    res.send({
+        csq: Math.floor(Math.random() * 6) + 1,
+        reg: Boolean((Math.floor(Math.random() * 2) + 1)),
+        data: Boolean((Math.floor(Math.random() * 2) + 1)),
+    })
 });
 
 app.listen(port, () => {
