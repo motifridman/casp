2ebd91b62f0f80583b90de0be491b84b1a6b9a1e
unknown <motifridman5@gmail.com>
2019-04-03 17:05:03 +0300

    duplicate

--- dist/models/modelsFill.js
+++ dist/models/modelsFill.js
@@ -39,7 +39,7 @@ function getUsers() {
     sms1 = {
         smsContact: contact1,
         smsType: models_1.SMSType.IncomingSMS,
-        text: "Hi!",
+        text: "Hi! Im looking for you, how are you today? are you feel ok? can you be here today? what do you you want to do tommoraw\r\nBay! have sdkfhbHi! Im looking for you, how are you today? are you feel ok? can you be here today? what do you you want to do tommoraw\r\nBay! have sdkfhb",
         time: Date.now()
     };
     let sms2;
@@ -97,6 +97,26 @@ function getUsers() {
         name: "Hike & Mike",
         number: "85471256254954"
     };
+    let contact7;
+    contact7 = {
+        name: "7Billy Bu",
+        number: "098554321"
+    };
+    let contact8;
+    contact8 = {
+        name: "8Hike & Mike",
+        number: "85471256254954"
+    };
+    let contact9;
+    contact5 = {
+        name: "9Billy Bu",
+        number: "098554321"
+    };
+    let contact10;
+    contact10 = {
+        name: "10Hike & Mike",
+        number: "85471256254954"
+    };
     let voiceCall4;
     voiceCall4 = {
         callContact: contact4,
@@ -115,6 +135,30 @@ function getUsers() {
         callType: models_1.VoiceCallType.IncomingCall,
         time: Date.now() - 30000
     };
+    let voiceCall7;
+    voiceCall7 = {
+        callContact: contact7,
+        callType: models_1.VoiceCallType.MissedCall,
+        time: Date.now() - 1000
+    };
+    let voiceCall8;
+    voiceCall8 = {
+        callContact: contact8,
+        callType: models_1.VoiceCallType.IncomingCall,
+        time: Date.now() - 300000
+    };
+    let voiceCall9;
+    voiceCall9 = {
+        callContact: contact9,
+        callType: models_1.VoiceCallType.MissedCall,
+        time: Date.now() - 1085700
+    };
+    let voiceCall10;
+    voiceCall10 = {
+        callContact: contact10,
+        callType: models_1.VoiceCallType.IncomingCall,
+        time: Date.now() - 1500
+    };
     let sms4;
     sms4 = {
         smsContact: contact5,
@@ -129,6 +173,20 @@ function getUsers() {
         text: "Hello world!\r\nNext line test",
         time: Date.now() - 189000
     };
+    let sms7;
+    sms7 = {
+        smsContact: contact6,
+        smsType: models_1.SMSType.OutgoingSMS,
+        text: "!Hello city",
+        time: Date.now() - 3910000
+    };
+    let sms8;
+    sms8 = {
+        smsContact: contact4,
+        smsType: models_1.SMSType.IncomingSMS,
+        text: "Hello world!\r\nNext line testHello world!\r\nNext line testHello world!\r\nNext line testHello world!\r\nNext line testHello world!\r\nNext line testHello world!\r\nNext line testHello world!\r\nNext line test",
+        time: Date.now() - 189000
+    };
     let sms6;
     sms6 = {
         smsContact: contact6,
@@ -138,10 +196,10 @@ function getUsers() {
     };
     let identity3;
     identity3 = {
-        contactsList: [contact4, contact5, contact6],
+        contactsList: [contact4, contact5, contact6, contact7, contact8, contact9, contact10],
         name: "Thired identity",
-        smsList: [sms4, sms5, sms6],
-        voiceCallsList: [voiceCall4, voiceCall5]
+        smsList: [sms4, sms5, sms6, sms7, sms8],
+        voiceCallsList: [voiceCall4, voiceCall5, voiceCall6, voiceCall7, voiceCall8, voiceCall9, voiceCall10]
     };
     let identity4;
     identity4 = {
@@ -162,11 +220,35 @@ function getUsers() {
         name: "Profile 4",
         lastName: "Last Name 4"
     };
+    let profile5;
+    profile5 = {
+        identitiesList: [identity3, identity4],
+        name: "Profile 5",
+        lastName: "Last Name 5"
+    };
+    let profile6;
+    profile6 = {
+        identitiesList: [identity3],
+        name: "Profile 6",
+        lastName: "Last Name 6"
+    };
+    let profile7;
+    profile7 = {
+        identitiesList: [identity3, identity4],
+        name: "Profile 7",
+        lastName: "Last Name 7"
+    };
+    let profile8;
+    profile8 = {
+        identitiesList: [identity3],
+        name: "Profile 8",
+        lastName: "Last Name 8"
+    };
     let user1;
     user1 = {
         name: "User1",
         passward: "1234",
-        profileList: [profile1, profile2, profile3, profile4],
+        profileList: [profile1, profile2, profile3, profile4, profile5, profile6, profile7, profile8],
         userType: models_1.UserType.standart
     };
     let user2;
--- server/models/modelsFill.ts
+++ server/models/modelsFill.ts
@@ -44,7 +44,7 @@ export function getUsers() : User[]  {
   sms1 = {
     smsContact: contact1,
     smsType: SMSType.IncomingSMS,
-    text: "Hi!",
+    text: "Hi! Im looking for you, how are you today? are you feel ok? can you be here today? what do you you want to do tommoraw\r\nBay! have sdkfhbHi! Im looking for you, how are you today? are you feel ok? can you be here today? what do you you want to do tommoraw\r\nBay! have sdkfhb",
     time: Date.now()
   };
 
@@ -112,6 +112,28 @@ export function getUsers() : User[]  {
     name: "Hike & Mike",
     number: "85471256254954"
   };
+  let contact7: Contact;
+  contact7 = {
+    name: "7Billy Bu",
+    number: "098554321"
+  };
+
+  let contact8: Contact;
+  contact8 = {
+    name: "8Hike & Mike",
+    number: "85471256254954"
+  };
+  let contact9: Contact;
+  contact5 = {
+    name: "9Billy Bu",
+    number: "098554321"
+  };
+
+  let contact10: Contact;
+  contact10 = {
+    name: "10Hike & Mike",
+    number: "85471256254954"
+  };
 
   let voiceCall4: VoiceCall;
   voiceCall4 = {
@@ -134,6 +156,34 @@ export function getUsers() : User[]  {
     time: Date.now() - 30000
   };
 
+  let voiceCall7: VoiceCall;
+  voiceCall7 = {
+    callContact: contact7,
+    callType: VoiceCallType.MissedCall,
+    time: Date.now() - 1000
+  };
+
+  let voiceCall8: VoiceCall;
+  voiceCall8 = {
+    callContact: contact8,
+    callType: VoiceCallType.IncomingCall,
+    time: Date.now() - 300000
+  };
+
+  let voiceCall9: VoiceCall;
+  voiceCall9 = {
+    callContact: contact9,
+    callType: VoiceCallType.MissedCall,
+    time: Date.now() - 1085700
+  };
+
+  let voiceCall10: VoiceCall;
+  voiceCall10 = {
+    callContact: contact10,
+    callType: VoiceCallType.IncomingCall,
+    time: Date.now() - 1500
+  };
+
   let sms4 : SMS;
   sms4 = {
     smsContact: contact5,
@@ -150,6 +200,22 @@ export function getUsers() : User[]  {
     time: Date.now() - 189000
   };
 
+  let sms7 : SMS;
+  sms7 = {
+    smsContact: contact6,
+    smsType: SMSType.OutgoingSMS,
+    text: "!Hello city",
+    time: Date.now() - 3910000
+  };
+
+  let sms8 : SMS;
+  sms8 = {
+    smsContact: contact4,
+    smsType: SMSType.IncomingSMS,
+    text: "Hello world!\r\nNext line testHello world!\r\nNext line testHello world!\r\nNext line testHello world!\r\nNext line testHello world!\r\nNext line testHello world!\r\nNext line testHello world!\r\nNext line test",
+    time: Date.now() - 189000
+  };
+
   let sms6 : SMS;
   sms6 = {
     smsContact: contact6,
@@ -160,10 +226,10 @@ export function getUsers() : User[]  {
 
   let identity3: Identity;
   identity3 = {
-    contactsList: [contact4, contact5, contact6],
+    contactsList: [contact4, contact5, contact6, contact7, contact8, contact9, contact10],
     name: "Thired identity",
-    smsList : [sms4, sms5, sms6],
-    voiceCallsList: [voiceCall4, voiceCall5]
+    smsList : [sms4, sms5, sms6, sms7, sms8],
+    voiceCallsList: [voiceCall4, voiceCall5, voiceCall6, voiceCall7, voiceCall8, voiceCall9, voiceCall10]
   };
 
   let identity4: Identity;
@@ -188,11 +254,39 @@ export function getUsers() : User[]  {
     lastName: "Last Name 4"
   };
 
+  let profile5: Profile;
+  profile5 = {
+    identitiesList: [identity3, identity4],
+    name: "Profile 5",
+    lastName: "Last Name 5"
+  };
+
+  let profile6: Profile;
+  profile6 = {
+    identitiesList: [identity3],
+    name: "Profile 6",
+    lastName: "Last Name 6"
+  };
+
+  let profile7: Profile;
+  profile7 = {
+    identitiesList: [identity3, identity4],
+    name: "Profile 7",
+    lastName: "Last Name 7"
+  };
+
+  let profile8: Profile;
+  profile8 = {
+    identitiesList: [identity3],
+    name: "Profile 8",
+    lastName: "Last Name 8"
+  };
+
   let user1: User;
   user1 = {
     name: "User1",
     passward: "1234",
-    profileList: [profile1, profile2, profile3, profile4],
+    profileList: [profile1, profile2, profile3, profile4, profile5, profile6, profile7, profile8],
     userType: UserType.standart
   }
 
